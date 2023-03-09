982210458305a132bf2294dd955e2918281b8bd1
gordonk <gpkarlsson72@gmail.com>
2023-03-08 19:57:54 -0600

    adding in seeds files that didn't get added in the last commit

---
+++ seeds/answers.js
@@ -0,0 +1,11 @@
+const { Answers } = require('../models');
+
+
+//TODO: Continue adding answers
+//? Figure out what other parameters go in here along with the answers
+const answerData = [
+  {
+    name: 'Answer 1'
+  }
+
+]
---
+++ seeds/questions.js
@@ -0,0 +1,8 @@
+const { Questions } = require('../models');
+
+//TODO: Add questions, figure out what other parameters are needed/how to implement. Do questions and answers need to be combined into one?
+const questionData = [
+  {
+    name: 'Question 1'
+  }
+]
