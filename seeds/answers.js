5e6d3a3de781fb26c9b651de801d7f395fb8607e
gordonk <gpkarlsson72@gmail.com>
2023-03-09 21:13:57 -0600

    working on questions seeds

--- seeds/answers.js
+++ seeds/answers.js
@@ -2,7 +2,7 @@ const { Answers } = require('../models');
 
 
 //TODO: Continue adding answers
-//? Figure out what other parameters go in here along with the answers
+//? Figure out what other parameters go in here along
 const answerData = [
   {
     name: 'Answer 1'
--- seeds/questions.js
+++ seeds/questions.js
@@ -3,6 +3,45 @@ const { Questions } = require('../models');
 //TODO: Add questions, figure out what other parameters are needed/how to implement. Do questions and answers need to be combined into one?
 const questionData = [
   {
-    name: 'Question 1'
+    name: 'Geography Quiz',
+    questions: [
+      'What continent is the country Georgia located in?',
+      'What is the largest state by area in the US?',
+      'What is the smallest state by area in the US?',
+      'What state borders Wisconsin to the south?',
+      'What country borders the US to the north?'
+    ],
+    answers: [
+      'Europe',
+      'Alaska',
+      'Rhode Island',
+      'Illinois',
+      'Canada'
+    ]
+  },
+  {
+    name: 'Math Quiz',
+    questions: [
+      'What is 2+2',
+      'What is the square root of 4',
+      'If 3x = 9, solve for x',
+      'What is 9000 divided by 3?',
+      'List the '
+    ],
+    answers: [
+      '4',
+      '2',
+      'x = 3',
+      '3000',
+      '3.14'
+    ]
+  },
+  {
+    name: 'JavaScript Quiz',
+    questions: [
+      'What does DOM stand for?',
+      'What does NPM stand for?',
+      ''
+    ]
   }
 ]
