0974f599781005869792452bc81dcea3031fea8b
AxelPanic <alexthickey@gmail.com>
2023-03-11 05:54:51 -0600

    adding to remote

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,13 +1,24 @@
 const router = require('express').Router();
+const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
-
-router.get('/', withAuth, (req, res) => {
-    console.log('HOME');
-    res.render('home', {
-        loggedIn: req.session.loggedIn
-    });
-});
+router.get('/', withAuth, async (req, res) => {
+    try {
+      const userData = await User.findAll({
+        attributes: { exclude: ['password'] },
+        order: [['name', 'ASC']],
+      });
+  
+      const users = userData.map((project) => project.get({ plain: true }));
+  
+      res.render('home', {
+        users,
+        logged_in: req.session.logged_in,
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 router.get('/login', (req, res) => {
     console.log('LOGIN');
