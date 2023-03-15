8d73d721444ab499a0e5e173d6271624608d438c
Sam Wright <samwright@Sams-MacBook-Pro-3.local>
2023-03-14 21:23:27 -0500

    Added additional question categories and questions to the questions.js file

--- seeds/questions.js
+++ seeds/questions.js
@@ -89,12 +89,93 @@ const questionData = [
         category: 'math',
         question: 'What are prime numbers divisible by?',
         answer: '1 and themselves'
-      }
-      ,
-      'If 3x = 9, solve for x',
-      'What is 9000 divided by 3?',
-      'What are prime numbers divisible by?',
-      'How many degrees must all angles in a triangle add up to?'
+      },
+    ],
+  },
+  {
+    name: 'History Quiz',
+    questions: [
+      {
+        category: 'history',
+        question: 'What year did the American Revolutionary war begin?',
+        answers: [
+          { text: '1776', correct: true},
+          { text: '1775', correct: false},
+          { text: '1867', correct: false},
+          { text: '1941', correct: false}
+        ]
+      },
+      {
+        category: 'history',
+        question: 'Who invented the lightbulb?',
+        answers: [
+          { text: 'George Washington', correct: false},
+          { text: 'Benjamin Franklin', correct: false},
+          { text: 'Albert Einstein', correct: false},
+          { text: 'Thomas Edison', correct: true}
+        ]
+      },
+      {
+        category: 'history',
+        question: 'Who discovered the Americas?',
+        answers: [
+          { text: 'Christopher Columbus', correct: true},
+          { text: 'Abraham Lincoln', correct: false},
+          { text: 'John Cabot', correct: false},
+          { text: 'Sir Francis Drake', correct: false}
+        ]
+      },
+      {
+        category: 'history',
+        question: 'What year did World War 2 end?',
+        answers: [
+          { text: '1914', correct: false},
+          { text: '1972', correct: false},
+          { text: '1945', correct: true},
+          { text: '1949', correct: false}
+        ]
+      },
+      {
+        category: 'history',
+        question: 'What was the last state to join the union in 1959?',
+        answers: [
+          { text: 'Hawaii', correct: true},
+          { text: 'Alaska', correct: false},
+          { text: 'Washington', correct: false},
+          { text: 'Texas', correct: false}
+        ]
+      },
+    ],
+  },
+  {
+    name: 'Language Arts Quiz',
+    questions: [
+      {
+        category: 'language arts',
+        question: 'A word that shares the same meaning as another word is known as a',
+        answer: 'synonym'
+      },
+      {
+        category: 'language arts',
+        question: 'The name of a person, place, or thing',
+        answer: 'noun'
+      },
+      {
+        category: 'language arts',
+        question: 'A word used to describe a noun or pronoun',
+        answer: 'adjective'
+      },
+      {
+        category: 'language arts',
+        question: 'This word is used to join words, phrases, or clauses such as "but and "or"',
+        answer: 'conjunction'
+      },
+      {
+        category: 'language arts',
+        question: 'Which term indicates a pause, typically between two main clauses, that is more pronounced than that indicated by a comma.',
+        answer: 'semicolon'
+      },
+    
 
     ],
   },
