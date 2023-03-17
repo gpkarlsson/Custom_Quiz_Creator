d9657b423604c7bc062ad3d27cbb893c76e1c8e2
AxelPanic <alexthickey@gmail.com>
2023-03-16 19:11:01 -0500

    answer added

---
+++ models/Answer.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const Quiz = require('./Quiz');
+
+class Answer extends Model { }
+
+Answer.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        quiz_title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        correct_answer: {
+            type: DataTypes.BOOLEAN,
+            references: {
+                model: "Quiz",
+                key: "id"
+            },
+        },
+    },
+
+
+
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'quiz',
+    });
+
+module.exports = Answer;
