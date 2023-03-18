ea99d22bcef1bb50162c0d20ee26534977ba9c93
Axelpanic <alexthickey@gmail.com>
2023-03-18 17:15:01 -0500

    fixing still

--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,15 @@
 const sequelize = require('../config/connection');
 const seedGeoQuestions = require('./geoquestions');
 
+//const seedFiller = require('./answers');
+
 //Seed Answers and Questions
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
   await seedGeoQuestions();
+
+  //await seedFiller();
 
   process.exit(0);
 };
