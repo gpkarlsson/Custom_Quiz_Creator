4ba7a834ea260f71035ff95f7b7ab222ef81daa5
AxelPanic <alexthickey@gmail.com>
2023-03-19 15:00:40 -0500

    fixing quiz handlebars

--- models/Quiz.js
+++ models/Quiz.js
@@ -1,12 +1,51 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Quiz extends Model {}
+class Quiz extends Model { }
 
-Quiz.init({
-    category: DataTypes.STRING,
-    questionCount: DataTypes.INTEGER,
-}, {sequelize, modelName: 'quiz'})
+Quiz.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subject: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_created: {
+      type: DataTypes.BOOLEAN,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    questions: {
+      type: DataTypes.STRING,
+      allowNull: false,
 
+    }
+  },
 
-module.exports = Quiz
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'quiz',
+  });
+
+module.exports = Quiz;
