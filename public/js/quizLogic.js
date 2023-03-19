26ca945d04ec90b41f583d494f29e1595b37bda7
gordonk <gpkarlsson72@gmail.com>
2023-03-19 15:49:09 -0500

    working on quiz logic

--- public/js/quizLogic.js
+++ public/js/quizLogic.js
@@ -1,23 +1,54 @@
-const {Questions, Quiz } = require('./models');
-// const 
-
+const { Questions, Quiz } = require('./models');
 
 const questionS = document.querySelector('#question');
 const answerS = document.querySelector('#answers');
-const sumbitBtn = document.querySelector('#submit');
-let currentquestionid = 0;
+const submitBtn = document.querySelector('#submit');
+let currentQuestionId = 0;
 let score = 0;
 
-function startQuiz() {
-    for (let i = 0; i < questionS.length, i++;) {
-        const question = questionS[i];
+async function startQuiz() {
+    // Retrieve the quiz data from the server
+    const quiz = await Quiz.findOne();
+    const questions = await Questions.find({ quizId: quiz._id });
+
+    // Compile the Handlebars templates
+    const questionTemplate = Handlebars.compile('{{question}}');
+    const answerTemplate = Handlebars.compile('<button class="answer">{{answer}}</button>');
+
+    // Disimage.pngplay the first question
+    displayQuestion(questions[currentQuestionId]);
+
+    function displayQuestion(question) {
+        // Render the question text using the Handlebars template
+        questionS.innerHTML = questionTemplate({ question: question.text });
+
+        // Render the answer buttons using the Handlebars template
+        answerS.innerHTML = question.answers.map(answer => answerTemplate({ answer })).join('');
+
+        // Add event listeners to the answer buttons
+        const answerButtons = answerS.querySelectorAll('.answer');
+        answerButtons.forEach(button => {
+            button.addEventListener('click', () => {
+                const cAnswer = question.answers.find(answer => answer.isCorrect);
+                if (button.textContent === cAnswer.text) {
+                    score++;
+                }
+                currentQuestionId++;
+                if (currentQuestionId < questions.length) {
+                    displayQuestion(questions[currentQuestionId]);
+                } else {
+                    endQuiz();
+                }
+            });
+        });
     }
-    function questionLoad() {
-        currentquestionid.answerS.forEach(answer => {
-            const btn = doument.addEventListener('click', userAnswer);
-            btn.textContent = answer;
-        })
+
+    function endQuiz() {
+        // Display the final score
+        questionS.innerHTML = `You scored ${score} out of ${questions.length} questions.`;
+        answerS.innerHTML = '';
+        submitBtn.disabled = true;
     }
 }
 
-// Need logic here to connect button to correct answer (cAnswer) and score.
+startQuiz();
